08ec357480eace9636884700d78247307812c93d
noder12345 <113690020+noder12345@users.noreply.github.com>
2023-04-27 16:46:27 +0300

    Add files via upload

---
+++ rotshild/js/questions.js
@@ -0,0 +1,98 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+     {
+    numb: 1,
+    question: "שאלה",
+    answer: "א",
+    options: [
+      "א",
+      "ב",
+      "ג",
+      "ד"
+     ]
+  },
+
+  {
+    numb: 2,
+    question: "1",
+    answer: "א",
+    options: [
+      "א",
+      "ב",
+      "ג",
+      "ד"
+     ]
+  },
+
+
+  {
+    numb: 3,
+    question: "שאלה",
+    answer: "א",
+    options: [
+      "א",
+      "ב",
+      "ג",
+      "ד"
+     ]
+  },
+
+  {
+    numb: 4,
+    question: "שאלה",
+    answer: "א",
+    options: [
+      "א",
+      "ב",
+      "ג",
+      "ד"
+     ]
+  },
+
+  {
+    numb: 5,
+    question: "שאלה",
+    answer: "א",
+    options: [
+      "א",
+      "ב",
+      "ג",
+      "ד"
+     ]
+  },
+
+  {
+    numb: 6,
+    question: "שאלה",
+    answer: "א",
+    options: [
+      "א",
+      "ב",
+      "ג",
+      "ד"
+     ]
+  },
+
+
+             
+
+
+
+]
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+  //{
+
+ 
